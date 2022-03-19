b11adaaf93770761fdebae0360ef696e48b64ced
Kevin Huang <huang.han.kev@gmail.com>
2022-03-19 17:11:36 +0000

    remove unnecessary files

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-      require('tailwindcss')('./tailwind.config.js'),
-      require('autoprefixer'),
-      ...(process.env.JEKYLL_ENV == "production"
-        ? [require('cssnano')({})]
-        : [])
-    ]
-}
